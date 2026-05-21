40f033bd5b1fe993ce3df64c0df93233ad816929
omarescobartorres10 <omar.escobartorres10@gmail.com>
2026-05-21 14:06:03 -0600

    style: diferentes estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React from "react";
 import './App.css'
+import JobCard from "./JobCard";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const jobs = [
+        {
+            id: 1,
+            title: "Desarrollador Web",
+            company: "Google",
+            location: "Ciudad de México",
+            salary: "$20,000 - $25,000",
+        },
+        {
+            id: 2,
+            title: "Frontend Developer",
+            company: "Microsoft",
+            location: "Remoto",
+            salary: "$25,000 - $30,000",
+        },
+        {
+            id: 3,
+            title: "Backend Developer",
+            company: "Google",
+            location: "Ciudad de México",
+            salary: "$20,000 - $25,000",
+        },
+        {
+            id: 4,
+            title: "AI Developer",
+            company: "Apple",
+            location: "Cupertino",
+            salary: "$5000 - $6000",
+        },
+    ]
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+<div className="jobs-grid">
+     <ul>
+    {jobs.map((job) => (
+        <JobCard 
+        key={job.id}
+        titulo={job.title}
+        empresa={job.company}
+        ubicacion={job.location}
+        salario={job.salary}
+        />
+    ))}
+  </ul>
+</div>
+ 
   )
 }
 
---
+++ src/JobCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./jobCard.css"
+
+
+
+function JobCard(props) {
+  return (
+    <div className="job-card">
+      <h3>{props.titulo}</h3>
+      <p>{props.empresa} | {props.ubicacion}</p>
+      <p>{props.salario}</p>
+      <button onClick={() => alert(`Aplicaste a ${props.titulo}`)}>
+        Aplicar
+      </button>
+    </div>
+  );
+}
+
+export default JobCard;
+
